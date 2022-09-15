857db9b9fcf16b21804024b5e95a7a884b711b1d
Alisher Bazarbay <aweka@Alishers-MacBook-Pro.local>
2022-09-15 17:43:14 +0800

    add comments

--- backward/computations_backward.py
+++ backward/computations_backward.py
@@ -3,18 +3,44 @@ from typing import Tuple
 from utils.derivatives import *
 from utils.backward_activations import *
 
-def get_derivative_respect_to_activation(A: np.array, Y: np.array, name: str = 'sigmoid'):
+def get_derivative_respect_to_activation(A: np.array, Y: np.array, name: str = 'sigmoid') -> np.array:
+    """Function to calculate the derivative repsect to activation function.
+
+     Arguments:
+    ----------
+    A -- probability vector
+    Y -- ground truth vector
+    name -- name of the activation function, used to get the function from utils.derivatives modiel
+
+    Returns:
+    ----------
+    dA -- vector of derivatives 
+    """
     derivative = derivatives_dict[name]
+    dA = derivative(A, Y)
+    return dA
 
-    return derivative(A, Y)
+def linear_activation_backward(dA: np.array, cache: Tuple[Tuple, np.array], name: str = 'relu') -> Tuple(np.array*3):
+    """Helper function to compute backpropagation.
+    
+    Arguments:
+    ----------
+    dA -- activation gradients for current layer l (size of previous layer, number of datapoints)
+    cache -- tuple of caches for computations (linear cache, activation cache)
+    name -- name of the activation fucntion for backward prop
 
-def linear_activation_backward(dA: np.array, cache: Tuple[Tuple, np.array], name: str = 'relu'):
+    Returns:
+    ----------
+    dW -- gradient: cost function respect to the bias (of current layer)
+    db -- gradient: cost function respect to the weights (of current layer)
+    dA -- gradient: cost function respect to the activation fucntion (of the previous layer)
+    """
     linear_cache, activation_cache = cache
 
     backward_activation = backward_activation_functions[name]
 
     dZ = backward_activation(dA, activation_cache)
-    
+
     A_previous, W, b = linear_cache
     m = A_previous.shape[1]
 
@@ -22,4 +48,4 @@ def linear_activation_backward(dA: np.array, cache: Tuple[Tuple, np.array], name
     db = np.sum(dZ, axis=1, keepdims=True) / m
     dA_previous = np.dot(W.T, dZ)
 
-    return [dW, db, dA_previous]
+    return dW, db, dA_previous
